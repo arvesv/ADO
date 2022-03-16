2d35f35f7a86ae341619df5b27edb6e94fbe8373
Arve Svendsen <arve.svendsen@gmail.com>
2022-03-16 05:33:36 +0100

    Cleanup and reformat

--- Program.cs
+++ Program.cs
@@ -1,88 +1,78 @@
-﻿using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
+﻿using System.Net.Http.Headers;
 using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json.Linq;
 
 var configuration = new ConfigurationBuilder()
-  .AddUserSecrets<Program>()
-  .Build();
+    .AddUserSecrets<Program>()
+    .Build();
 
 // This program uses 2 secrets.
 // ADO:PAT - Access token
 // ADO:Org - Name of azure devops org
 
+var personalAccessToken = configuration["ADO:PAT"];
 
-string personalAccessToken = configuration["ADO:PAT"]; //sl3i6omo73ieoecquewe2nfllsj56tnnnzm4t6ohv4jr7j3attbq;
 
-
+HttpResponseMessage response;
 using (var client = new HttpClient())
 {
-    client.BaseAddress = new Uri($"https://vssps.dev.azure.com/{configuration["ADO:Org"]}/");  //url of your organization
+    client.BaseAddress = new Uri($"https://vssps.dev.azure.com/{configuration["ADO:Org"]}/"); //url of your organization
     client.DefaultRequestHeaders.Accept.Clear();
-    client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
     //encode your personal access token                   
-    string credentials = Convert.ToBase64String(System.Text.ASCIIEncoding.ASCII.GetBytes(string.Format("{0}:{1}", "", personalAccessToken)));
+    var credentials =
+        Convert.ToBase64String(System.Text.Encoding.ASCII.GetBytes(string.Format("{0}:{1}", "", personalAccessToken)));
     client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", credentials);
 
-    string conttoken = "";
+    var conttoken = "";
     //connect to the REST endpoint     
 
-    HttpResponseMessage response;
-    IEnumerable<string> continuationTokens = null;
+    var readersdesc = new List<string>();
+    var qaDesc = "";
 
-    List<string> readersdesc = new List<string>();
-    string qaDesc = "";
+    bool morestuff;
+    do
+    {
+        if (string.IsNullOrEmpty(conttoken))
+            response = client.GetAsync("_apis/graph/groups?api-version=5.1-preview.1").Result;
+        else
+            response = client.GetAsync($"_apis/graph/groups?continuationToken={conttoken}&api-version=5.1-preview.1")
+                .Result;
 
-    bool morestuff = false;
-    do {       
-        if(string.IsNullOrEmpty(conttoken)) {
-            response = client.GetAsync( "_apis/graph/groups?api-version=5.1-preview.1").Result;
-        } else {
-            response = client.GetAsync( $"_apis/graph/groups?continuationToken={conttoken}&api-version=5.1-preview.1").Result;          
-        }
+        if (!response.IsSuccessStatusCode) throw new Exception();
 
-        if(!response.IsSuccessStatusCode) {
-             throw(new Exception());
-        }
-
-        if(response.Headers.TryGetValues("x-ms-continuationtoken", out continuationTokens)) {
+        if (response.Headers.TryGetValues("x-ms-continuationtoken", out var continuationTokens))
+        {
             conttoken = continuationTokens.FirstOrDefault();
             morestuff = true;
         }
-        else 
+        else
         {
             morestuff = false;
         }
 
         var value = response.Content.ReadAsStringAsync().Result;
-        JObject i = JObject.Parse(value);
+        var i = JObject.Parse(value);
 
-        for(int z = 0; z < ((int)i["count"]); z++) {
+        for (var z = 0; z < (int)i["count"]; z++)
+        {
+            var name = (string)i["value"][z]["displayName"];
 
-            string name = ((string)i["value"][z]["displayName"]);
 
+            if (name == "RDTeamQualityTools") qaDesc = (string)i["value"][z]["descriptor"];
 
-            if(name == "RDTeamQualityTools") {
-                qaDesc = ((string)i["value"][z]["descriptor"]);
-            }
-
-            if(name == "Readers")
+            if (name == "Readers")
             {
                 Console.WriteLine($"  Reader  {i["value"][z]["principalName"]}");
 
                 readersdesc.Add((string)i["value"][z]["descriptor"]);
             }
         }
-    } while(morestuff);
+    } while (morestuff);
 
 
-    foreach(var s in readersdesc) {
-
-        var re = client.PutAsync($"_apis/graph/memberships/{qaDesc}/{s}?api-version=5.1-preview.1", null).Result;
+    foreach (var s in readersdesc)
+    {
+        var unused = client.PutAsync($"_apis/graph/memberships/{qaDesc}/{s}?api-version=5.1-preview.1", null).Result;
     }
-
-
-
-
 }
